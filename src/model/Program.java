8c212ad4575465b204222054f24a6ff49c441723
DyneRidian <jonathan.walkden@gmail.com>
2016-10-02 23:11:52 +0200

    Revert "Revert "deleteBoat() method fixes and functionality added to interface""
    This reverts commit cfce2a6e3f5e4033d306c3c8ffbb799a9f18a857.

--- src/model/Program.java
+++ src/model/Program.java
@@ -9,6 +9,12 @@ public class Program {
 		Admin a = new Admin();
 		a.populate();
 		
-		a.deleteBoat("1", "1");
+		//a.addBoat("1", new Kayak(null,null), "5");
+		
+		a.deleteBoat("4","4");
+		
+		//System.out.println(a.getMemberStorage().get(0).getBoatList().get(0).boatType());
+		//System.out.println(a.getMemberStorage().get(0).getBoatList().get(1).boatType());
+		
 	}
 }
